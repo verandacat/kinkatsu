bda41e6422e115373203c5018f583bd5a6a7a7b7
verandacat <verandacats@yahoo.co.jp>
2021-12-13 14:24:45 +0900

    全てのページindex_a.phpに設定変更

--- index copy.php
+++ index copy.php
@@ -3,21 +3,21 @@ $_SESSION['switch'] = $_GET['switch'];
 if($_GET['pat'] == "a"){
 include_once('./index_a.php');
 }elseif($_GET['pat'] == "b"){
-include_once('./index_a.php');
+include_once('./index_b.php');
 }elseif($_GET['pat'] == "c"){
-include_once('./index_a.php');
+include_once('./index_c.php');
 }elseif($_GET['pat'] == "d"){
-include_once('./index_a.php');
+include_once('./index_d.php');
 }elseif($_GET['pat'] == "e"){
-include_once('./index_a.php');
+include_once('./index_e.php');
 }elseif($_GET['pat'] == "f"){
-include_once('./index_a.php');
+include_once('./index_f.php');
 }elseif($_GET['pat'] == "g"){
-include_once('./index_a.php');
+include_once('./index_g.php');
 }elseif($_GET['pat'] == "h"){
-include_once('./index_a.php');
+include_once('./index_h.php');
 }elseif($_GET['pat'] == "i"){
-include_once('./index_a.php');
+include_once('./index_i.php');
 }else{
 include_once('./index_a.php');
 }
--- iphone/index.php
+++ iphone/index.php
@@ -3,21 +3,21 @@ $_SESSION['switch'] = $_GET['switch'];
 if($_GET['pat'] == "a"){
 include_once('./index_a.php');
 }elseif($_GET['pat'] == "b"){
-include_once('./index_b.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "c"){
-include_once('./index_c.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "d"){
-include_once('./index_d.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "e"){
-include_once('./index_e.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "f"){
-include_once('./index_f.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "g"){
-include_once('./index_g.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "h"){
-include_once('./index_h.php');
+include_once('./index_a.php');
 }elseif($_GET['pat'] == "i"){
-include_once('./index_i.php');
+include_once('./index_a.php');
 }else{
 include_once('./index_a.php');
 }
